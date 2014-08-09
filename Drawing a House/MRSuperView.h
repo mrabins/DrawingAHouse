cbf56f925a130be93b647b7b5a5db9ca96981d40
markrabins <markrabins@gmail.com>
2014-08-09 12:31:03 -0700

    first commit

---
+++ Drawing a House/MRSuperView.h
@@ -0,0 +1,13 @@
+//
+//  MRSuperView.h
+//  Drawing a House
+//
+//  Created by Mark Rabins on 7/16/14.
+//  Copyright (c) 2014 edu.self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRSuperView : UIView
+
+@end
---
+++ Drawing a House/MRSuperView.m
@@ -0,0 +1,31 @@
+//
+//  MRSuperView.m
+//  Drawing a House
+//
+//  Created by Mark Rabins on 7/16/14.
+//  Copyright (c) 2014 edu.self.edu. All rights reserved.
+//
+
+#import "MRSuperView.h"
+
+@implementation MRSuperView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
