90240a7a72f0c50ee3287caa7c3fcf78d7c54397
markrabins <markrabins@gmail.com>
2014-08-17 13:17:57 -0700

    white screen fix

--- Drawing a House/MRSuperView.m
+++ Drawing a House/MRSuperView.m
@@ -19,13 +19,22 @@
     return self;
 }
 
-/*
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
-    // Drawing code
+    UIBezierPath *bezierPath = [UIBezierPath bezierPath];
+    bezierPath.lineWidth = 2.0;
+    [bezierPath moveToPoint:CGPointMake(1/4.0 * self.bounds.size.width, 1/3.0 * self.bounds.size.height)];
+    [bezierPath addLineToPoint:CGPointMake(1/4.0 * self.bounds.size.width, 2/3.0 * self.bounds.size.height)];
+    [bezierPath addLineToPoint:CGPointMake(3/4.0 * self.bounds.size.width, 2/3.0 * self.bounds.size.height)];
+    [bezierPath addLineToPoint:CGPointMake(3/4.0 * self.bounds.size.width, 1/3.0 * self.bounds.size.height)];
+    [bezierPath addLineToPoint:CGPointMake(1/2.0 * self.bounds.size.width, 1/4.0 * self.bounds.size.height)];
+    [bezierPath closePath];
+    [bezierPath stroke];
+    
 }
-*/
+
 
 @end
